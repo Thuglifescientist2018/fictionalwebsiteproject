9104e6b8c5b8d404d0c7b00ec7261c3637a85f19
= <=>
2020-05-28 13:28:33 +0545

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+const logo = document.querySelector(".logo").addEventListener("click", function(){
+    document.getElementById('home').scrollIntoView({});
+});
+
+
+$("#navbar a, .btn").on("click", function(event){
+    if(this.hash !== '') {
+      event.preventDefault();
+      const hash  = this.hash;
+      $('html, body').animate(
+        {
+          scrollTop: $(hash).offset().top - 100
+        },
+        800
+      );
+    }
+  });
+  
